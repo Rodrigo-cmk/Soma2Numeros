2f5df43c89645df41504454b0df90b5160c23953
Rodrigo Alves <rodrigorac.2003@gmail.com>
2021-04-01 13:13:10 -0300

    Soma de 2 Números

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+            int valor, valor2;
+            int resul;
+
+
+            Console.WriteLine();
+            Console.ForegroundColor=ConsoleColor.DarkBlue;
+            Console.WriteLine("Cálculo da soma entre dois números.");
+            Console.WriteLine(" --- Insira dois valores inteiros a serem somados ---");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            valor = Convert.ToInt16 (Console.ReadLine());
+            Console.WriteLine("+");
+            valor2 = Convert.ToInt16 (Console.ReadLine());
+            Console.WriteLine();
+
+            resul = valor + valor2;
+
+            Console.WriteLine($"Soma: {resul}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
